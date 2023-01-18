221b89a656ee6747e555e86b676fa4e49c8a0f3d
abauer726 <abauer726@gmail.com>
2023-01-18 14:43:09 -0800

    Update stock_v5.py

--- tf/stock_v5.py
+++ tf/stock_v5.py
@@ -31,42 +31,6 @@ def sim_gbm(x0, model):
             (model['r'] - model['div'][j] - model['sigma'][j]**2/2)*dt))
     return np.reshape(np.ravel(np.array(newX)), (length, model['dim']), order='F')
 
-def QMC_neo(n, t_step, factor, quasimc, model):
-    '''
-    Space-filling on a specified hyper-rectangular
-    
-    Parameters
-    ----------
-    n : Integer
-    factor : Float 
-    quasimc : String recording the type
-              'Sobol'
-    model : Dictionary containing all the parameters, including volatility, 
-            interest rate, and continuous dividend yield
-        
-    Returns
-    -------
-    Array 
-    '''
-    ### DELETE Plot
-    ### plt.scatter(igen.transpose()[0], igen.transpose()[1])
-    nSteps = int(model['T']/model['dt'])    # Number of Steps
-    t_step = int(t_step/model['T']*nSteps)
-    if t_step > nSteps:
-        raise ValueError('Time step is equal to or exceeds maturity.')
-    if quasimc == 'Sobol':
-        sampler = qmc.Sobol(d=model['dim'], scramble=False)
-    elif quasimc == 'Halton':
-        sampler = qmc.Halton(d=model['dim'], scramble=False)
-    elif quasimc == 'LatinHypercube':
-        sampler = qmc.LatinHypercube(d=model['dim'])
-    # sampler.fast_forward(n*t_step)
-    input_range = model['x0']*factor
-    sampler.fast_forward(random.randint(0, n**3))
-    sample = model['x0'] - input_range + 2*input_range*sampler.random(n)
-    # Select only in-the-money
-    return np.delete(sample, np.where(payoff(sample, model) == 0), axis = 0)
-
 def stock_sim(nSims, model, start = None):
     '''
     Simulates stock paths using Geometric Brownian Motion
@@ -94,6 +58,76 @@ def stock_sim(nSims, model, start = None):
     for i in range(1,nSteps):
         test_j.append(sim_gbm(test_j[i-1], model))
     return np.reshape(np.ravel(test_j), (nSteps, nSims, model['dim']))
+
+def QMC_range(n, input_range, quasimc, model, all=False):
+    '''
+    Space-filling on a specified hyper-rectangular
+    
+    Parameters
+    ----------
+    n : Integer
+    factor : Float 
+    quasimc : String recording the type
+              'Sobol'
+              'Halton'
+              'LatinHypercube'
+    model : Dictionary containing all the parameters, including volatility, 
+            interest rate, and continuous dividend yield
+    all : Boolean 
+            True : displays all the values 
+            False : displays only in-the-money entries
+    Returns
+    -------
+    Array 
+    '''
+    nSteps = int(model['T']/model['dt'])    # Number of Steps
+    if quasimc == 'Sobol':
+        sampler = qmc.Sobol(d=model['dim'], scramble=False)
+    elif quasimc == 'Halton':
+        sampler = qmc.Halton(d=model['dim'], scramble=False)
+    elif quasimc == 'LatinHypercube':
+        sampler = qmc.LatinHypercube(d=model['dim'])
+    dim_range = []
+    for j in range(model['dim']):
+        dim_range.append(input_range[j][1] - input_range[j][0])
+    dim_range = np.asarray(dim_range, dtype=float)
+    sampler.fast_forward(random.randint(0, nSteps*n))
+    sample = input_range.transpose()[0] + dim_range*sampler.random(n)
+    # Select only in-the-money
+    if all == False:
+        return np.delete(sample, np.where(payoff(sample, model) == 0), axis = 0)
+    else:
+        return sample
+
+def stock_sim_stop(nSims, model, stop = None):
+    '''
+    Simulates stock paths using Geometric Brownian Motion
+    
+    Parameters
+    ----------
+    nSims : Integer with the number of simulations -- N
+    model : Dictionary containing all the parameters, including volatility, 
+            interest rate, and continuous dividend yield
+    stop : Float with stop time. The default is None.
+
+    Returns
+    -------
+    An arrayof shape M x N x model['dim']
+    where M is the number of steps model['T']/model['dt']
+    '''
+    start = np.reshape(np.repeat(model['x0'], nSims), (nSims, model['dim']))
+    # nSteps = int(model['T']/model['dt'])
+    if stop == None:
+        stop = 0
+    else:
+        if not(np.round(stop,4) in np.round(np.arange(model['dt'], model['T']+model['dt'], model['dt']),4)):
+            raise TypeError('Time step is out of range for the model dt.')
+        stop = int(round(stop/model['dt']))
+    test_j = []
+    test_j.append(sim_gbm(start, model))
+    for i in range(1,stop):
+        test_j.append(sim_gbm(test_j[i-1], model))
+    return np.reshape(np.ravel(test_j), (stop, nSims, model['dim']))
 
 def stock_sim_neo(nSims, t_point, x_point, model):
     '''
@@ -126,7 +160,7 @@ def stock_sim_neo(nSims, t_point, x_point, model):
         test_j.append(sim_gbm(test_j[i-1], model))
     return np.reshape(np.ravel(test_j), (nSteps-step, nSims, model['dim']))
 
-def stock_sim_v3(t_point, start, model):
+def stock_sim_v5(t_point, start, model):
     '''
     Simulates stock paths using Geometric Brownian Motion
     
@@ -145,15 +179,16 @@ def stock_sim_v3(t_point, start, model):
         raise TypeError('x_points needs to be a Numpy Array.')
     if start.shape[1] != model['dim']:
         raise ValueError('x_points  does not match the dimension of the model.')
-    if not(t_point in np.round(np.arange(0, model['T'], model['dt']), 5)):
+    if not(np.round(t_point,4) in np.round(np.arange(model['dt'], model['T'], model['dt']), 4)):
         raise ValueError('t_point is not in the proper range.')
+    
     nSteps = int(model['T']/model['dt'])
     step = int(np.round(t_point/model['dt']))
-    test_j = []
-    test_j.append(sim_gbm(start, model))
-    for i in range(1,nSteps-step):
-        test_j.append(sim_gbm(test_j[i-1], model))
-    return np.reshape(np.ravel(test_j), (nSteps-step, len(start), model['dim']))
+    
+    test_j = [start]
+    for _ in range(nSteps-step):
+        test_j.append(sim_gbm(test_j[-1], model))
+    return np.reshape(np.ravel(test_j), (nSteps-step+1, len(start), model['dim']))
 
 
 def stock_thin(stock, model, dt):
